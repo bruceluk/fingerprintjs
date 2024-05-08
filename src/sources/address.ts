b392f342448f7ec98fb01f663e70ef82a4f587ca
xiaoshifeifei <1521318212@qq.com>
2024-05-08 15:51:07 +0800

    展示公网ip

--- src/sources/address.ts
+++ src/sources/address.ts
@@ -1,4 +1,3 @@
-
 export interface PublicIp {
   publicIp: string
   localIp: string
@@ -11,67 +10,95 @@ export interface PublicIp {
  * Canvas image is noised in private mode of Safari 17, so image rendering is skipped in Safari 17.
  */
 export default function getAddress(): Promise<PublicIp> {
-  // console.log('1344444---------', getUnstableCanvasFingerprint(doesBrowserPerformAntifingerprinting()))
-
   return getUnstableAddress()
 }
-export async function getPublicIp(callback: any) {
-  var ip_dups: any = {}
-  var RTCPeerConnection = window.RTCPeerConnection
-  var servers = {
-    iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
-  }
-  var pc = new RTCPeerConnection(servers)
-  function handleCandidate(candidate: any) {
-    var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
-    var ip_addr: any = ip_regex.exec(candidate)
-    if (ip_addr) {
-      ip_addr = ip_addr[1]
-    }
-    if (ip_dups[ip_addr] === undefined) {
-      if (typeof callback === 'function') {
-        sessionStorage.setItem('publicIp', ip_addr)
-        return callback(ip_addr)
-      }
-    }
-  }
-  pc.onicecandidate = function (ice) {
-    if (ice.candidate) {
-      handleCandidate(ice.candidate.candidate)
-    } else if (ice.candidate) {
-    }
-  }
-  pc.createDataChannel('')
-  pc.createOffer(
-    function (result) {
-      pc.setLocalDescription(
-        result,
-        function () {},
-        function () {},
-      )
-    },
-    function () {},
-  )
-  setTimeout(function () {
-    var lines = pc.localDescription?.sdp.split('\n')
-    lines?.forEach(function (line) {
-      if (line.indexOf('a=candidate:') === 0) {
-        handleCandidate(line)
-      }
+// export async function getPublicIp(callback: any) {
+//   var ip_dups: any = {}
+//   var RTCPeerConnection = window.RTCPeerConnection
+//   var servers = {
+//     iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
+//   }
+//   var pc = new RTCPeerConnection(servers)
+//   function handleCandidate(candidate: any) {
+//     var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
+//     var ip_addr: any = ip_regex.exec(candidate)
+//     if (ip_addr) {
+//       ip_addr = ip_addr[1]
+//     }
+//     if (ip_dups[ip_addr] === undefined) {
+//       if (typeof callback === 'function') {
+//         return callback(ip_addr)
+//       }
+//     }
+//     return ip_addr
+//   }
+//   pc.onicecandidate = function (ice) {
+//     if (ice.candidate) {
+//       handleCandidate(ice.candidate.candidate)
+//     } else if (ice.candidate) {
+//     }
+//   }
+//   pc.createDataChannel('')
+//   pc.createOffer(
+//     function (result) {
+//       pc.setLocalDescription(
+//         result,
+//         function () {},
+//         function () {},
+//       )
+//     },
+//     function () {},
+//   )
+//   setTimeout(function () {
+//     var lines = pc.localDescription?.sdp.split('\n')
+//     lines?.forEach(function (line) {
+//       if (line.indexOf('a=candidate:') === 0) {
+//         handleCandidate(line)
+//       }
+//     })
+//   }, 1000)
+// }
+// getPublicIp(function (ip: any) {
+//   console.log(ip)
+// })
+function getPublicIp(): any {
+  return new Promise((resolve) => {
+    const pc = new RTCPeerConnection({
+      iceServers: [{ urls: 'stun:stun.l.google.com:19302' }],
     })
-  }, 1000)
+    function handleCandidate(candidate: any) {
+      var ip_regex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/
+      var ip_addr: any = ip_regex.exec(candidate)
+      ip_addr = ip_addr[1]
+      resolve(ip_addr)
+    }
+    pc.onicecandidate = function (ice) {
+      if (ice.candidate) {
+        handleCandidate(ice.candidate.candidate)
+      } else if (ice.candidate) {
+      }
+    }
+    pc.createDataChannel('')
+    pc.createOffer(
+      function (result) {
+        pc.setLocalDescription(
+          result,
+          function () {},
+          function () {},
+        )
+      },
+      function () {},
+    )
+  })
 }
-/**
- * A version of the entropy source without stabilization.
- *
- * Warning for package users:
- * This function is out of Semantic Versioning, i.e. can change unexpectedly. Usage is at your own risk.
- */
 
 export async function getUnstableAddress(): Promise<PublicIp> {
-  let publicIp: any
+  // let publicIp: any = ''
   let localIp: string = await getLocalIP()
-  publicIp = sessionStorage.getItem('publicIp')
+  let publicIp: any = await getPublicIp()
+
+  console.log('publicIp---------', publicIp)
+
   return { publicIp, localIp }
 }
 
@@ -80,7 +107,6 @@ function getLocalIP(): any {
     const peerConnection = new RTCPeerConnection({
       iceServers: [],
     })
-
     peerConnection.createDataChannel('')
     peerConnection
       .createOffer()
@@ -98,4 +124,3 @@ function getLocalIP(): any {
       })
   })
 }
-
